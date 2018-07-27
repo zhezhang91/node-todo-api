78eb3dff8033a0cc35eea8fc6e4b5dcd3e131d50
zhezhang91 <zz188659@gmail.com>
2018-07-27 16:30:08 -0400

    Init commit

---
+++ play/mongob-connect.js
@@ -0,0 +1,42 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27018/TodoApp', (err,client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDb server');
+    }
+    console.log('Connected to MongoDb server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').insertOne({
+    //    text: 'Something doing',
+    //    completed: false
+    // }, (err, result) => {
+    //     if(err) {
+    //         return console.log('Unable to insert todo', err)
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // })
+
+    // const db = client.db('User');
+    // db.collection('Users').insertOne({
+    //    name: 'Andrew',
+    //    age: 25,
+    //    location: 'Ottawa'
+    // }, (err, result) => {
+    //     if(err) {
+    //         return console.log('Unable to insert user', err)
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // })
+
+    db.collection('Todos').find().count().then( (count) => {
+        console.log(`Todos count: ${count}`);
+    //    console.log(JSON.stringify(, undefined, 2));
+    }, (err) => {
+        console.log(err)
+    })
+
+    // client.close();
+})
