117a1c26db1a67eda7c39c632217bff60f23f672
zhezhang91 <zz188659@gmail.com>
2018-08-07 13:29:53 -0400

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,17 @@ app.post('/todos', (req, res) => {
     .catch(e => res.status(400).send(e));
 })
 
+app.get('/todos', (req,res) => {
+    Todo.find().then(todos => {
+        res.send({
+            todos,
+        })
+    })
+    .catch(e => {
+        res.status(400).send(e)
+    })
+})
+
 app.listen(4000, () => {
     console.log('Started on port 4000');
 });
