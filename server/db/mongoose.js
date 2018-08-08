b7cd9a5592713d634b99bd37070993b09fb1f038
zhezhang91 <zz188659@gmail.com>
2018-08-08 12:03:51 -0400

    Setup seperate test and dev envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 // tell mongoose to use Promise
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
    mongoose 
 }
+
--- server/server.js
+++ server/server.js
@@ -1,3 +1,13 @@
+const env = process.env.NODE_ENV || 'development';
+console.log('env @@@@@@@', env)
+if(env === 'development') {
+  process.env.PORT = 4000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test') {
+    process.env.PORT = 4000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
+
 const express = require('express');
 const _ = require('lodash');
 const bodyParser = require('body-parser');
